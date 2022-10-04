0678f3250eba8ccc9cedc1b078b3fa22aa45dc78
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-10-04 13:28:22 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors')
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+var path = require('path');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+  res.json('tttt')
+
+})
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+
+const PORT = process.env.PORT
+server.listen(PORT, () => console.log("Servidor inicialitzat"))
+
